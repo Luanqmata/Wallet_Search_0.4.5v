9a7753be8344f23905c6477e9e7e6f8b80397c4e
Aprendendo C# / Banco de Dados <138558778+Luanqmata@users.noreply.github.com>
2024-09-25 04:56:28 -0300

    0.4.5v

--- main.go
+++ main.go
@@ -5,23 +5,17 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
+	"math/big"
 	"meugo/encoding"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 )
 
 const (
-	prefix        = "00000000000000000000000000000000000000000000000" // Prefixo da chave
-	memBufferSize = 2 * 1024 * 1024 * 1024                            // usando 2gb pro buffer
+	memBufferSize = 4 * 1024 * 1024 * 1024 // usando 4GB pro buffer
 )
-
-var chaves_desejadas = map[string]bool{
-	"1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9": true, //67
-	"1MVDYgVaSN6iKKEsbzRUAYFrYJadLYZvvZ": true, //68
-}
-
-// 4 byte xx xx xx xx
 
 var (
 	contador          int
@@ -29,21 +23,54 @@ var (
 	mu                sync.Mutex
 	wg                sync.WaitGroup
 	ultimaChaveGerada string
-	memBuffer         = make([]byte, memBufferSize)
+	memBuffer         = make([]byte, memBufferSize) //metodo 1 usando a const
+	//memBuffer          = make([]byte, 4*1024*1024*1024) // metodo 2 apagando a const
+	startTime          time.Time
+	tamanhoChave       int
+	carteiras          map[int]string
+	ranges             map[int]string
+	minRange           *big.Int
+	maxRange           *big.Int
+	carteira_escolhida string
 )
 
-func gerarChavePrivada() string {
-	suffix := make([]byte, 9) // se for carteira impar coloca 1 byte a mais e no enconde ajusta com o tamanho de carcters da key
-	_, err := rand.Read(suffix)
-	if err != nil {
-		log.Fatalf("Falha ao gerar chave: %v", err)
+func init() {
+	carteiras = make(map[int]string)
+	ranges = make(map[int]string)
+}
+
+func setupRanges() {
+	rangeStr, existe := ranges[tamanhoChave]
+	if !existe {
+		log.Fatalf("Só existem 160 chaves. %d este número não é aceito.", tamanhoChave)
 	}
-	chaveGerada := prefix + hex.EncodeToString(suffix)[:17] //linha usada para gerar somente 17carcters
 
-	// Manipulação eficiente do buffer (exemplo genérico)
-	copy(memBuffer[:len(suffix)], suffix)
+	valores := strings.Split(rangeStr, "-")
+	minRange = new(big.Int)
+	minRange.SetString(valores[0], 16)
+	maxRange = new(big.Int)
+	maxRange.SetString(valores[1], 16)
+}
 
-	return chaveGerada
+func gerarChavePrivada() string {
+	var chaveGerada *big.Int
+	for {
+		chaveGerada, _ = rand.Int(rand.Reader, new(big.Int).Sub(maxRange, minRange))
+		chaveGerada.Add(chaveGerada, minRange)
+
+		if chaveGerada.Cmp(minRange) >= 0 && chaveGerada.Cmp(maxRange) <= 0 {
+			break
+		}
+	}
+
+	chaveHex := hex.EncodeToString(chaveGerada.Bytes())
+	if len(chaveHex) < 64 {
+		chaveHex = strings.Repeat("0", 64-len(chaveHex)) + chaveHex
+	}
+
+	copy(memBuffer[:len(chaveHex)], chaveHex)
+
+	return chaveHex
 }
 
 func worker(id int) {
@@ -60,16 +87,13 @@ func worker(id int) {
 		chave := gerarChavePrivada()
 		pubKeyHash := encoding.CreatePublicHash160(chave)
 		address := encoding.EncodeAddress(pubKeyHash)
-
 		mu.Lock()
 		contador++
 		ultimaChaveGerada = chave
-		//fmt.Print("\n", ultimaChaveGerada) //mostrar Private keys
-		//fmt.Print("\n", address) // mostrar Carteira
-		if chaves_desejadas[address] {
-			fmt.Printf("\n\n|--------------%s----------------|\n", address)
-			fmt.Printf("|----------------------ATENÇÃO-PRIVATE-KEY-----------------------|")
-			fmt.Printf("\n|%s|\n", chave)
+		if address == carteira_escolhida {
+			fmt.Printf("\n\n	|--------------%s----------------|\n", address)
+			fmt.Printf("	|----------------------ATENÇÃO-PRIVATE-KEY-----------------------|")
+			fmt.Printf("\n	|%s|\n", chave)
 			encontrado = true
 			mu.Unlock()
 			return
@@ -79,27 +103,53 @@ func worker(id int) {
 }
 
 func main() {
+	encoding.CarregarRangesDoArquivo("enderecos/ranges.txt", ranges)
+	encoding.CarregarRangesDoArquivo("enderecos/carteiras.txt", carteiras)
+
 	fmt.Print("\n\n\n\n\n\n")
 	fmt.Println(`                                              	        			-_~ BEM VINDO ~_-
 						________         __  __         __       _______                            __
 						|  |  |  |.---.-.|  ||  |.-----.|  |_    |     __|.-----..---.-..----..----.|  |--.
 						|  |  |  ||  _  ||  ||  ||  -__||   _|   |__     ||  -__||  _  ||   _||  __||     |
 						|________||___._||__||__||_____||____|   |_______||_____||___._||__|  |____||__|__|  ~ 0.3.9v  By:Ch!iNa ~
-
-							
-									  -_~ Carteira Puzzle: 66 67 ~_-
+									  
+									    -_~ :Carteira Puzzle: ~_-
 	`)
+
+	fmt.Print("\n\nDigite qual Carteira você vai querer procurar: ")
+	var escolha_carteira_chave int
+	fmt.Scanln(&escolha_carteira_chave)
+
+	if carteira, ok := carteiras[escolha_carteira_chave]; ok {
+		carteira_escolhida = carteira
+		fmt.Printf("\nCarteira escolhida: %s (bits: %d)\n", carteira_escolhida, escolha_carteira_chave)
+	} else {
+		fmt.Printf("Número de carteira %d não suportado. Escolha um valor entre 1 e %d.\n", escolha_carteira_chave, len(carteiras))
+		return
+	}
+
+	tamanhoChave = escolha_carteira_chave
+	setupRanges()
+
+	if _, ok := ranges[tamanhoChave]; !ok {
+		fmt.Printf("	Tamanho de chave %d não suportado. Escolha um valor entre 1 e %d.\n", tamanhoChave, len(ranges))
+		return
+	}
+
 	time.Sleep(1 * time.Second)
-	fmt.Println("\n\n	Obs: Computador do criador tem 28 threads... (KIT XEON | 2.40 ~ 3.00 GHz | E5 2680 v4)")
+	fmt.Println("\n\n	Obs: Computador do criador tem 28 threads... (Processador Intel® Xeon® E5-2680 v4)")
 	fmt.Println("\n\n\n						- Random Mode -")
-	fmt.Println("\n\n		Modo 1: Easy   (15%) - CPU 56°C - 125 RPM  - 86k  Chaves P/seg")
-	fmt.Println("		Modo 2: Seguro (25%) - CPU 60°C - 170 RPM  - 150k Chaves P/seg")
-	fmt.Println("		Modo 3: Medium (50%) - CPU 73°C - 220 RPM+ - 322k Chaves P/seg")
-	fmt.Println("		Modo 4: Hard   (75%) - CPU 78°C - 255 RPM+ - 415k Chaves P/seg")
-	fmt.Println("\n\n					- - - A partir daqui ja está fritando o CPU - - -")
-	fmt.Println("\n\n		Modo 5: Um pouquinho pra daqui apoco !!PERIGO!! (94%) - CPU 80°C. - 450k Chaves P/seg")
+
+	fmt.Println("\n\n		Modo 1: Easy   (15%) - CPU 60°C - 125 RPM  - 86k  Chaves P/seg")
+	fmt.Println("		Modo 2: Seguro (25%) - CPU 65°C - 170 RPM  - 150k Chaves P/seg")
+	fmt.Println("		Modo 3: Medium (50%) - CPU 78°C - 220 RPM (+) - 322k Chaves P/seg")
+	fmt.Println("		Modo 4: Hard   (75%) - CPU 83°C - 255 RPM (+) - 415k Chaves P/seg")
+	fmt.Println("\n\n					- - - A partir daqui já está fritando o CPU - - -")
+
+	fmt.Println("\n\n		Modo 5: Um pouquinho pra daqui a pouco !!PERIGO!! (94%) - CPU 80°C. - 450k Chaves P/seg")
 	fmt.Println("		Modo 6: Não Estou nem Aí para meu computador quero que ele queime (100%) - CPU 85°C. - 468k Chaves P/seg")
-	fmt.Print("\n\n	Input: Escolha o modo de acordo com o número correspondente: ")
+
+	fmt.Print("\n\n Input: Escolha o modo de acordo com o número correspondente: ")
 
 	var escolha int
 	fmt.Scanln(&escolha)
@@ -122,14 +172,15 @@ func main() {
 		fmt.Println("	Escolha inválida. Usando o SECURE MODE...  (20%) - CPU 58°C - 117K Chaves P/seg.")
 		numThreads = 4
 	}
-	fmt.Printf("\n  		Iniciando modo %d .", escolha)
+	fmt.Printf("\n  		Threads: %d iniciando .", numThreads)
 	time.Sleep(1 * time.Second)
-	fmt.Printf("\n  		Iniciando modo %d ..", escolha)
+	fmt.Printf("\n  		Threads: %d iniciando ..", numThreads)
 	time.Sleep(1 * time.Second)
-	fmt.Printf("\n  		Iniciando modo %d ...\n\n", escolha)
+	fmt.Printf("\n  		Threads: %d iniciando ...\n\n", numThreads)
 	time.Sleep(1 * time.Second)
 
 	runtime.GOMAXPROCS(numThreads)
+	startTime = time.Now()
 
 	// Inicia goroutines
 	for i := 0; i < numThreads; i++ {
@@ -145,19 +196,9 @@ func main() {
 				mu.Unlock()
 				break
 			}
-			fmt.Printf("\r 	N° Threads Usados: %d | Chaves Geradas: %d | ", numThreads, contador)
-			mu.Unlock()
-		}
-	}()
-
-	go func() {
-		for {
-			time.Sleep(2 * time.Second)
-			mu.Lock()
-			if encontrado {
-				mu.Unlock()
-				break
-			}
+			duration := time.Since(startTime).Seconds()
+			chavesPorSegundo := float64(contador) / duration
+			fmt.Printf("\r  N° Threads Usados: %d | Chaves Geradas: %d | Chaves Por Segundo: %.2f |", numThreads, contador, chavesPorSegundo)
 			fmt.Print("Ultima Chave Gerada: ", ultimaChaveGerada, " |")
 			mu.Unlock()
 		}
@@ -165,7 +206,7 @@ func main() {
 
 	wg.Wait()
 
-	fmt.Print("\n\n	|--------------------------------------------------by-Luan-BSC---|")
+	fmt.Print("	|--------------------------------------------------by-Luan-BSC---|")
 	fmt.Print("\n	|-----------------------China-LOOP-MENU------------------------- |")
 	fmt.Printf("\n	|		Threads usados: %d		                 |", numThreads)
 	fmt.Print("\n	|		Chaves Analisadas:	", contador)
